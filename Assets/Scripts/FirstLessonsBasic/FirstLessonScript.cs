152e92828d33146227a353d9b7676c42b640fb1b
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-02-13 01:58:02 +0300

    +Added: new First Lessons Scripts & Rotate Object Scripts

---
+++ Assets/Scripts/FirstLessonsBasic/FirstLessonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FirstLessonsBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {
+        //private bool isTrue=false;
+        //[SerializeField] private int counter = 0;
+        /// <summary>
+        /// Variables
+        /// C#
+        /// </summary>
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/FirstLessonsBasic/Lessons/RotateObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    public class RotateObject : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 1f;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _speed*Time.deltaTime);
+        }
+    }
+}
+
+
