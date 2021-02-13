d7d6a177f24a0c46b8cf5d136aca6adcaa6e07b9
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-02-13 12:06:55 +0300

    +Added: PhysicMaterial Bounce, Control Rigidbody Script, Rigidbody Setting Script, Rigidbody Setting Default Asset +Refactoring: Rotate Setting Slow Asset

---
+++ Assets/Scripts/FirstLessonsBasic/Lessons/ControlRigidbody.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace FirstLessonsBasic.Lessons
+{
+    public class ControlRigidbody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private RigidbodySettings _settings;
+
+        private void Update()
+        {
+            bool spaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+            if (spaceKeyDown)
+            {
+                _rigidbody.AddForce(_settings.JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FirstLessonsBasic/Lessons/RigidbodySettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace FirstLessonsBasic.Lessons
+{
+    [CreateAssetMenu(menuName = "Lessons/Lesson1/Control Rigidbody Settings")]
+    public class RigidbodySettings : ScriptableObject
+    {
+        [SerializeField] private Vector3 _jumpForce;
+
+        public Vector3 JumpForce => _jumpForce;
+    }
+}
