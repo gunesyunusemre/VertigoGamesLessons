36c83d6f998524ba261e8a095e0f601014c2cbe1
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-02-13 12:53:59 +0300

    +Added: Basic Movement For Camera

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace TopDownShooter.Scripts.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraSettings _cameraSettings;
+        [SerializeField] private Transform _targetTransform;
+        [SerializeField] private Transform _cameraTransform;
+
+        private void Update()
+        {
+            CameraRotationFollow();
+            CameraMovementFollow();
+        }
+
+        private void CameraRotationFollow()
+        {
+            _cameraTransform.rotation=Quaternion.Lerp(_cameraTransform.rotation,
+                Quaternion.LookRotation(_targetTransform.position-_cameraTransform.position),
+                Time.deltaTime*_cameraSettings.RotationLerpSpeed);
+        }
+
+        private void CameraMovementFollow()
+        {
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position,
+                _targetTransform.position+_cameraSettings.PositionOffset,
+                Time.deltaTime*_cameraSettings.PositionLerpSpeed);
+        }
+    }
+}
+
---
+++ Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TopDownShooter.Scripts.Camera
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float _rotationLerpSpeed = 1;
+        public float RotationLerpSpeed => _rotationLerpSpeed;
+
+        [Header("Position")]
+        [SerializeField] private Vector3 _positionOffset;
+
+        public Vector3 PositionOffset => _positionOffset;
+        
+        [SerializeField] private float _positionLerpSpeed = 1;
+
+        public float PositionLerpSpeed => _positionLerpSpeed;
+    }
+}
+
