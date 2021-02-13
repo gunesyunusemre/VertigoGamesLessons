e10707938aacee92485c673dea8b78ac1b2b9ad0
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-02-13 14:19:30 +0300

    Adding: Input Data Script, Input Manager Script

---
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace TopDownShooter.Scripts.Input
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Input/Input Data")]
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+    }
+}
---
+++ Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace TopDownShooter.Scripts.Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private InputData _inputData;
+
+        private void Update()
+        {
+            _inputData.Horizontal = UnityEngine.Input.GetAxis("Horizontal");
+            _inputData.Vertical = UnityEngine.Input.GetAxis("Vertical");
+        }
+    }
+}
