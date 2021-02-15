1615cd0b24f60ad93405738723dff8fe32ee5f80
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-02-15 19:11:12 +0300

    +refactoring: movement folder move to player control folder

---
+++ Assets/TopDownShooter/Scripts/PlayerControl/Movement/PlayerMovementController.cs
@@ -0,0 +1,22 @@
+﻿using TopDownShooter.Scripts.Input;
+using UnityEngine;
+
+namespace TopDownShooter.Scripts.PlayerControl
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private PlayerMovementSettings _playerMovementSettings;
+
+        private void Update()
+        {
+            _rigidbody.MovePosition(_rigidbody.position+(_rigidbody.transform.forward * 
+                                    (_inputData.Vertical * _playerMovementSettings.VerticalSettings)));
+            _rigidbody.MovePosition(_rigidbody.position+(_rigidbody.transform.right * 
+                                    (_inputData.Horizontal * _playerMovementSettings.HorizontalSettings)));
+            //_rigidbody.MovePosition(_rigidbody.position+(_rigidbody.transform.up * 
+            //                                             (_inputData.Jump*_playerMovementSettings.JumpSettings)));
+        }
+    }
+}
---
+++ Assets/TopDownShooter/Scripts/PlayerControl/Movement/PlayerMovementSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace TopDownShooter.Scripts.PlayerControl
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Player/Movement Settings")]
+    public class PlayerMovementSettings : ScriptableObject
+    {
+        [SerializeField] private float _horizontalSettings=5;
+        public float HorizontalSettings => _horizontalSettings;
+        
+        [SerializeField] private float _verticalSettings=5;
+        public float VerticalSettings => _verticalSettings;
+
+        [SerializeField] private float _jumpSettings = 1;
+        public float JumpSettings => _jumpSettings;
+    }
+}
