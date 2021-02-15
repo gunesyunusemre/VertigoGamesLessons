8dc667b3c53643515498543c7f2fa308d0844f0a
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-02-15 19:10:20 +0300

    +adding: tower rotation control

---
+++ Assets/TopDownShooter/Scripts/PlayerControl/TowerRotation/TowerRotationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TopDownShooter.Scripts.Input;
+using UnityEngine;
+
+namespace TopDownShooter.Scripts.PlayerControl
+{
+    public class TowerRotationController : MonoBehaviour
+    {
+        [SerializeField] private InputData _rotationInput;
+        [SerializeField] private Transform _towerTransform;
+        [SerializeField] private TowerRotationSetting _towerRotationSetting;
+
+        private void Update()
+        {
+            _towerTransform.Rotate(0,
+                _rotationInput.Horizontal*_towerRotationSetting.TowerRotationSpeed,
+                0,Space.Self);
+        }
+    }
+}
---
+++ Assets/TopDownShooter/Scripts/PlayerControl/TowerRotation/TowerRotationSetting.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TopDownShooter.Scripts.PlayerControl
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Player/Tower Rotation Settings")]
+    public class TowerRotationSetting : ScriptableObject
+    {
+        public float TowerRotationSpeed = 1f;
+    }
+}
