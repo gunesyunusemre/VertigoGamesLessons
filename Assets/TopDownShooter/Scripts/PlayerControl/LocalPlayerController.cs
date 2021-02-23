915aeb593eb98c50dbec5f9707a22792065cc1e6
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-02-23 20:39:39 +0300

    +adding: local player scripts

---
+++ Assets/TopDownShooter/Scripts/PlayerControl/LocalPlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TopDownShooter.Scripts.Input;
+using TopDownShooter.Scripts.Inventory;
+using UnityEngine;
+
+namespace TopDownShooter.Scripts.PlayerControl
+{
+    public class LocalPlayerController : MonoBehaviour
+    {
+        [SerializeField]private PlayerInventoryController _InventoryController;
+        [SerializeField] private InputData _shootInput;
+
+        private void Update()
+        {
+            if (_shootInput.Horizontal > 0)
+            {
+                _InventoryController.ReactiveShootCommand.Execute();
+            }
+        }
+    }
+}
